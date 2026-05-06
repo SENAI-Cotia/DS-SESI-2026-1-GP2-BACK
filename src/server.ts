79553559c0cd9467c1b5ba6bf6d516af8399b524
meneses-hash <sexyjoaocaco@gmail.com>
2026-05-06 15:05:57 -0300

    entrega 5

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,10 @@ import userRouter from "./routes/usuario.router"
 import 'dotenv/config';
 
 const app = express();
-app.use(userRouter)
+
 
 app.use(express.json());
+app.use(userRouter)
 
 app.listen(3000, () => {
     console.log(`Server is running on port ${3000}`);
